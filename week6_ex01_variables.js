0a1c618a13cf2968107cfa22299377391b617374
Saabir Moefar <saabirmoefar456@gmail.com>
2026-05-18 21:13:19 +0200

    Create week6_ex01_variables.js

---
+++ week6_ex01_variables.js
@@ -0,0 +1,14 @@
+// Exercise 01 - Variables and String Manipulation
+
+const firstName = "Saabir";
+const lastName = "Moefar";
+let age = 19;
+
+const fullName = firstName + " " + lastName;
+
+console.log(
+  "Hello, my name is " + fullName + " and I am " + age + " years old.",
+);
+
+age = 20;
+console.log("Next year I will be " + age + " years old.");
