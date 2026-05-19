b93f5f407a5849360e4a667b206828225f5bb598
Saabir Moefar <saabirmoefar456@gmail.com>
2026-05-19 08:23:20 +0200

    Add Exercise 02 - Number Operations and Type Conversion

---
+++ week6_ex02_operations_types.js
@@ -0,0 +1,18 @@
+let number1 = 1
+let number2 = 2
+
+let number1string = String(number1)
+
+let sum = number1 + number2
+
+let product = number1 * number2
+
+console.log(number1)
+
+console.log(number2)
+
+console.log(number1string)
+
+console.log(sum)
+
+console.log(product)
