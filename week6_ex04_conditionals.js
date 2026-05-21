ea8d21187559896710c97eb4828572d2d817aa7a
Saabir Moefar <saabirmoefar456@gmail.com>
2026-05-21 12:03:25 +0200

    Add Exercise 04 - Conditional Logic

---
+++ week6_ex04_conditionals.js
@@ -0,0 +1,14 @@
+let score = 85
+
+let passinggrade = 70
+
+if(score === 100)
+    console.log ("perfect score! You passed")
+
+}
+esle if (socre >= passinggrade) {
+    console.log ("You passed")
+}
+else {
+    console.log ("You failed")
+}
